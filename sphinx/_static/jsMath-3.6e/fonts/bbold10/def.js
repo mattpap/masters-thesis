c5c120123784e98869090dba6798554faaeddfa3
Mateusz Paprocki <mattpap@gmail.com>
2010-05-03 21:27:58 +0200

    Added extensions and fonts to jsMath

---
+++ sphinx/_static/jsMath-3.6e/fonts/bbold10/def.js
@@ -0,0 +1,200 @@
+/*
+ *  bbold10/def.js
+ *  
+ *  This file loads the  font into jsMath. To do this use
+ *  
+ *      <SCRIPT>jsMath.Font.Load("bbold10")</SCRIPT>
+ *
+ *  after loading jsMath.js itself.  The user will need to have
+ *  the bbold10.ttf font installed, otherwise corresponding unicode
+ *  characters will be used, but the mapping is not perfect, and it is not 
+ *  customized on a per-browser basis as it probably should be.
+ *  
+ *  If the font isn't installed, the user will receive a message indicating
+ *  that fact, and pointing to the jsMath web site where the font can be
+ *  downloaded.
+ *  
+ *  Once this file is loaded, you can use \char{bbold10}{nn} to access
+ *  any character in the font.  In addition, the \bbold10 macro will switch to
+ *  the eufm font
+ *  
+ *  ---------------------------------------------------------------------
+ *
+ *  jsMath is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  jsMath is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with jsMath; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+jsMath.Add(jsMath.TeX,{
+
+  bbold10: [
+    // 00 - 0F
+    [0.611,0.683],
+    [0.611,0.683],
+    [0.667,0.683],
+    [0.611,0.683],
+    [0.667,0.683],
+    [0.667,0.683],
+    [0.667,0.683],
+    [0.611,0.683],
+    [0.667,0.683],
+    [0.611,0.683],
+    [0.667,0.683],
+    [0.556,0.431],
+    [0.544,0.694,0.194],
+    [0.472,0.431,0.194],
+    [0.472,0.694],
+    [0.472,0.431],
+    // 10 - 1F
+    [0.417,0.694,0.194],
+    [0.572,0.431,0.194],
+    [0.472,0.694],
+    [0.279,0.431],
+    [0.544,0.431],
+    [0.472,0.694],
+    [0.572,0.431,0.194],
+    [0.5,0.431],
+    [0.417,0.694,0.194],
+    [0.517,0.431],
+    [0.5,0.431,0.194],
+    [0.556,0.431],
+    [0.472,0.431],
+    [0.528,0.431],
+    [0.472,0.694,0.194],
+    [0.472,0.431,0.194],
+    // 20 - 2F
+    [0.528,0.694,0.194],
+    [0.334,0.683],
+    [0.558,0.694],
+    [0.778,0.683,0.194],
+    [0.611,0.767,0.0833],
+    [0.611,0.75,0.319],
+    [0.778,0.683],
+    [0.279,0.694,0,{lig: {'39': 34}}],
+    [0.362,0.75,0.25],
+    [0.362,0.75,0.25],
+    [0.688,0.583,0.0833],
+    [0.778,0.583,0.0833],
+    [0.279,0.168,0.194],
+    [0.417,0.431,0,{lig: {'45': 123}}],
+    [0.279,0.168],
+    [0.556,0.75,0.25],
+    // 30 - 3F
+    [0.556,0.644],
+    [0.556,0.644],
+    [0.556,0.644],
+    [0.556,0.644],
+    [0.556,0.644],
+    [0.556,0.644],
+    [0.556,0.644],
+    [0.556,0.644],
+    [0.556,0.644],
+    [0.556,0.644],
+    [0.279,0.431],
+    [0.279,0.431,0.194],
+    [0.833,0.519,0.0191],
+    [0.279,0.334],
+    [0.833,0.519,0.0191],
+    [0.611,0.683],
+    // 40 - 4F
+    [0.534,0.683],
+    [0.611,0.683],
+    [0.639,0.683],
+    [0.667,0.683],
+    [0.694,0.683],
+    [0.611,0.683],
+    [0.611,0.683],
+    [0.667,0.683],
+    [0.722,0.683],
+    [0.334,0.683],
+    [0.639,0.683],
+    [0.639,0.683],
+    [0.611,0.683],
+    [0.722,0.683],
+    [0.722,0.683],
+    [0.667,0.683],
+    // 50 - 5F
+    [0.639,0.683],
+    [0.667,0.683,0.194],
+    [0.639,0.683],
+    [0.611,0.683],
+    [0.611,0.683],
+    [0.722,0.683],
+    [0.611,0.683],
+    [0.833,0.683],
+    [0.667,0.683],
+    [0.611,0.683],
+    [0.667,0.683],
+    [0.362,0.75,0.25],
+    [0.556,0.75,0.25],
+    [0.362,0.75,0.25],
+    [0.362,0.75,0.25],
+    [0.362,0.75,0.25],
+    // 60 - 6F
+    [0.279,0.694,0,{lig: {'96': 126}}],
+    [0.5,0.431],
+    [0.628,0.694],
+    [0.472,0.431],
+    [0.5,0.694],
+    [0.472,0.431],
+    [0.389,0.694],
+    [0.5,0.431,0.194],
+    [0.572,0.694],
+    [0.279,0.669],
+    [0.389,0.669,0.194],
+    [0.544,0.694],
+    [0.279,0.694],
+    [0.722,0.431],
+    [0.572,0.431],
+    [0.472,0.431],
+    // 70 - 7F
+    [0.628,0.431,0.194],
+    [0.5,0.431,0.194],
+    [0.544,0.431],
+    [0.389,0.431],
+    [0.417,0.694],
+    [0.528,0.431],
+    [0.472,0.431],
+    [0.667,0.431],
+    [0.472,0.431],
+    [0.472,0.431,0.194],
+    [0.472,0.431],
+    [0.444,0.431,0,{lig: {'45': 125}}],
+    [0.334,0.75,0.25],
+    [0.833,0.431],
+    [0.558,0.694],
+    [0.667,0.431]
+  ]
+
+});
+jsMath.Setup.EncodeFont('bbold10');
+
+jsMath.Font.Register({
+  name: 'bbold10',
+  prefix: 'jsMath-',
+  // The test used to see if font is available
+  test: jsMath.Font.Test2, testChar: 0x3D, testFactor: 1.8,
+  // Can add style, styles, macros here
+  tex: function (font,fam) {
+    // do browser-specific adjustments here
+  },
+  fallback: function (font,fam) {
+    // set up unicode font here, e.g.
+    //    jsMath.Update.TeXfonts({
+    //      bbold10: {
+    //         '0': {c: '&#x221B;'},  // use code point U221B for character 0
+    //        '10': {c: '&#x210C;'}   // use code point U210C for character 10
+    //      }
+    //    });
+  }
+});
