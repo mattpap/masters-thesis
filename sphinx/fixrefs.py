bc96d05394f669eb3ff5906176972b73a9bf1082
Mateusz Paprocki <mattpap@gmail.com>
2010-06-15 07:01:58 +0200

    Added tools for generating latex

---
+++ sphinx/fixrefs.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python
+
+import sys
+
+INPUT = sys.argv[1]
+
+with open(INPUT) as f:
+    s = f.read()
+
+i = 0
+
+while 1:
+    i = s.find('\\hypertarget', i+1)
+
+    if i == -1:
+        break
+
+    a = s.find('{', i)
+    b = s.find('}', i)
+
+    ref = s[a+1:b]
+
+    j = b + 1
+    cnt = 0
+
+    while 1:
+        if s[j] == '}':
+            cnt -= 1
+
+            if not cnt:
+                break
+        elif s[j] == '{':
+            cnt += 1
+
+        j += 1
+
+    if s[j+1:j+15] != '\\begin{figure}':
+        continue
+
+    k = s.find('\\caption', i)
+
+    if k == -1:
+        continue
+
+    cnt = 0
+
+    while 1:
+        if s[k] == '}':
+            cnt -= 1
+
+            if not cnt:
+                break
+        elif s[k] == '{':
+            cnt += 1
+
+        k += 1
+
+    s = s[:k] + ('\\label{%s}' % ref) + s[k:]
+    s = s[:i] + s[j+1:]
+
+i = 0
+
+while 1:
+    i = s.find('\\hyperlink', i+1)
+
+    if i == -1:
+        break
+
+    a = s.find('{', i)
+    b = s.find('}', i)
+
+    ref = s[a+1:b]
+
+    j = b + 1
+    cnt = 0
+
+    while 1:
+        if s[j] == '}':
+            cnt -= 1
+
+            if not cnt:
+                break
+        elif s[j] == '{':
+            cnt += 1
+
+        j += 1
+
+    s = s[:i] + ('\\ref{%s}' % ref) + s[j+1:]
+
+i = 0
+
+while 1:
+    i = s.find('\\hypertarget', i+1)
+
+    if i == -1:
+        break
+
+    a = s.find('{', i)
+    b = s.find('}', i)
+
+    ref = s[a+1:b]
+
+    j = b + 1
+    cnt = 0
+
+    while 1:
+        if s[j] == '}':
+            cnt -= 1
+
+            if not cnt:
+                break
+        elif s[j] == '{':
+            cnt += 1
+
+        j += 1
+
+    if s[j+1:j+10] != '\\section{':
+        continue
+
+    k = j + 1
+    cnt = 0
+
+    while 1:
+        if s[k] == '}':
+            cnt -= 1
+
+            if not cnt:
+                break
+        elif s[k] == '{':
+            cnt += 1
+
+        k += 1
+
+    s = s[:k+1] + ('\\label{%s}' % ref) + s[k+1:]
+    s = s[:i] + s[j+1:]
+
+with open(INPUT, 'w') as f:
+    f.write(s)
+
---
+++ sphinx/translate.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+ARG=$1
+INPUT="$ARG.tex"
+
+sed -i -e "s/⋅/*/g" $INPUT
+sed -i -e "s/⎠/\//g" $INPUT
+sed -i -e "s/⎡/[/g" $INPUT
+sed -i -e "s/⎢/[/g" $INPUT
+sed -i -e "s/⎣/[/g" $INPUT
+sed -i -e "s/⎤/]/g" $INPUT
+sed -i -e "s/⎥/]/g" $INPUT
+sed -i -e "s/⎦/]/g" $INPUT
+sed -i -e "s/⎧/ /g" $INPUT
+sed -i -e "s/⎨/\\{/g" $INPUT
+sed -i -e "s/⎩/ /g" $INPUT
+sed -i -e "s/⎫/ /g" $INPUT
+sed -i -e "s/⎬/\\}/g" $INPUT
+sed -i -e "s/⎭/ /g" $INPUT
+sed -i -e "s/⎚/ /g" $INPUT
+sed -i -e "s/⎛/\//g" $INPUT
+sed -i -e "s/⎜/|/g" $INPUT
+sed -i -e "s/⎝/@textbackslash[]/g" $INPUT
+sed -i -e "s/⎞/@textbackslash[]/g" $INPUT
+sed -i -e "s/⎟/|/g" $INPUT
+sed -i -e "s/─/-/g" $INPUT
+sed -i -e "s/╱/\//g" $INPUT
+sed -i -e "s/╲/@textbackslash[]/g" $INPUT
+sed -i -e "s/₀/0/g" $INPUT
+sed -i -e "s/₁/1/g" $INPUT
+sed -i -e "s/₂/2/g" $INPUT
+sed -i -e "s/₃/3/g" $INPUT
+sed -i -e "s/₄/4/g" $INPUT
+sed -i -e "s/₅/5/g" $INPUT
+sed -i -e "s/₆/6/g" $INPUT
+sed -i -e "s/₇/7/g" $INPUT
+sed -i -e "s/₈/8/g" $INPUT
+sed -i -e "s/₉/9/g" $INPUT
+
+sed -i -e "/resetcurrentobjects/d" $INPUT
+sed -i -e "/begin{thebibliography}/,/end{thebibliography}/ d" $INPUT
+
+sed -i -e "s/hyperlink{[^}]\+}{{\[}\([^{]\+\){]}}/cite{\1}/g" $INPUT
+
+sed -i -e "/end{Verbatim}$/{N; s/$/\\\noindent/}" $INPUT
+
+sed -i -e "s/\\\end{document}/\\\bibliographystyle{acm}\n\\\bibliography{$ARG}\n\\\end{document}/" $INPUT
+
